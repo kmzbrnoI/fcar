717f733f6e06dc453caa21acfe404474034b4772
Jan Horacek <me@apophis.cz>
2020-10-09 18:33:31 +0200

    predmesti-viz: make coils work.

--- predmesti-viz/visualize.py
+++ predmesti-viz/visualize.py
@@ -12,6 +12,7 @@ def darken_probes(root):
         if item.attrib['id'].startswith('FH'):
             item.set('style', 'fill:#00ff00;fill-opacity:0.3;')
 
+SVG_ROOT = './/{http://www.w3.org/2000/svg}'
 
 
 if __name__ == '__main__':
@@ -19,30 +20,37 @@ if __name__ == '__main__':
     root = tree.getroot()
     darken_probes(root)
 
-    with serial.Serial('/dev/ttyUSB0', 9600, timeout=1) as ser:
-        line = ser.readline()
-        path = re.search(r'^\d*: Path (.*?): (\d*), (\d*)$')
-        magnet = re.search(r'^\d*: Magnet (.*?): (\d*)$')
+    with serial.Serial('/dev/ttyACM0', 9600) as ser:
+        while True:
+            line = ser.readline().decode('utf-8').strip()
+            path = re.search(r'^\d*: Path (.*?): (\d*), (-?\d*)$', line)
+            magnet = re.search(r'^\d*: Magnet (.*?): (\d*)$', line)
 
-        if path:
-            pid, pstate, pvehicle = path.groups()
-            item = root.find(f".//{http://www.w3.org/2000/svg}path[@id={pid}]")
-            COLORS = {
-                0: '#555555'
-                1: '#0000ff'
-                2: '#ff0000'
-                3: '#00ffff'
-                4: '#ffff00'
-            }
-            item.set('style', f'fill:{COLORS[pstate]};')
-            tree.write(open('test.svg', 'wb'))
+            if path:
+                pid, pstate, pvehicle = path.groups()
+                item = root.find(f'{SVG_ROOT}path[@id="{pid}"]')
+                if item is not None:
+                    COLORS = {
+                        0: '#555555',
+                        1: '#0000ff',
+                        2: '#ff0000',
+                        3: '#00ffff',
+                        4: '#ffff00',
+                    }
+                    item.set('style', f'fill:{COLORS[pstate]};')
+                    tree.write(open('test.svg', 'wb'))
+                else:
+                    print(f'ERROR: unknown path: {pid}!')
 
-        elif magnet:
-            mid, mstate = path.groups()
-            item = root.find(f".//{http://www.w3.org/2000/svg}path[@id={mid}]")
-            color = '#ff0000' if mstate == 0 else '#00ff00';
-            item.set('style', f'fill:{color};')
-            tree.write(open('test.svg', 'wb'))
+            elif magnet:
+                mid, mstate = [magnet.group(1), int(magnet.group(2))]
+                item = root.find(f'{SVG_ROOT}path[@id="{mid}"]')
+                if item is not None:
+                    color = '#ff0000' if mstate == 0 else '#00ff00';
+                    item.set('style', f'fill:{color};')
+                    tree.write(open('test.svg', 'wb'))
+                else:
+                    print(f'ERROR: unknown magnet: {mid}!')
 
-        else:
-            print(line)
+            else:
+                print(line)
