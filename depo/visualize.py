71749af081d9c1d0cd5010052f6e99426c059db4
Jan Horacek <me@apophis.cz>
2021-08-23 15:37:38 +0200

    visualize.py: do not reboot Arduino on connect.

--- depo/visualize.py
+++ depo/visualize.py
@@ -5,7 +5,7 @@ import serial
 
 if __name__ == '__main__':
 
-    with serial.Serial('/dev/ttyACM0', 9600) as ser:
+    with serial.Serial('/dev/ttyACM0', 9600, rtscts=False, xonxoff=False) as ser:
         print('Started...')
         while True:
             line = ser.readline().decode('utf-8').strip()
