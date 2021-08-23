5ae82e06d57c86f390e26eaa115f98eb1d4ce24c
celestian <petr.celestian@gmail.com>
2021-08-23 18:59:56 +0200

    Format of bus_stop

--- clang-format.sh
+++ clang-format.sh
@@ -4,6 +4,9 @@ RELATIVE_FCAR_SOURCE_PATH=`dirname ${BASH_SOURCE[@]}`
 FCAR_SOURCE_PATH=`realpath ${RELATIVE_FCAR_SOURCE_PATH}`
 
 clang-format -i -style=file \
+    `find ${FCAR_SOURCE_PATH}/bus_stop -type f -name '*.ino'` \
+    `find ${FCAR_SOURCE_PATH}/bus_stop -type f -name '*.cpp'` \
+    `find ${FCAR_SOURCE_PATH}/bus_stop -type f -name '*.h'`\
     `find ${FCAR_SOURCE_PATH}/depo -type f -name '*.ino'` \
     `find ${FCAR_SOURCE_PATH}/depo -type f -name '*.cpp'` \
     `find ${FCAR_SOURCE_PATH}/depo -type f -name '*.h'`
